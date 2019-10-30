7e7df400797bb7cab44aa15e2920d2ae66d73b08
Giulio Biondi <giulio.biondi.it@gmail.com>
2019-10-30 17:03:33 +0800

    Fixed QL agent.

--- sumo_rl/agents/ql_agent.py
+++ sumo_rl/agents/ql_agent.py
@@ -6,7 +6,6 @@ from sumo_rl.exploration.epsilon_greedy import EpsilonGreedy
 class QLAgent:
 
     def __init__(self, starting_state, state_space, action_space, alpha=0.5, gamma=0.95, exploration_strategy=EpsilonGreedy()):
-        super(QLAgent, self).__init__(state_space, action_space)
         self.state = starting_state
         self.state_space = state_space
         self.action_space = action_space
@@ -21,12 +20,12 @@ class QLAgent:
         self.action = self.exploration.choose(self.q_table, self.state, self.action_space)
         return self.action
 
-    def learn(self, next_state, reward, done=False):
-        if next_state not in self.q_table:
-            self.q_table[next_state] = [0 for _ in range(self.action_space.n)]
+    def learn(self, new_state, reward, done=False):
+        if new_state not in self.q_table:
+            self.q_table[new_state] = [0 for _ in range(self.action_space.n)]
 
         s = self.state
-        s1 = next_state
+        s1 = new_state
         a = self.action
         self.q_table[s][a] = self.q_table[s][a] + self.alpha*(reward + self.gamma*max(self.q_table[s1]) - self.q_table[s][a])
         self.state = s1
